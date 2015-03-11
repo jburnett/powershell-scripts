0b7741ada5d02ef00090a3cb21bd10d296b8f272
J Burnett <jburnett@altamodatech.com>
2015-03-11 12:07:21 -0400

    Prep for future versions of Visual Studio

--- Get-VSCommonToolsPath.ps1
+++ Get-VSCommonToolsPath.ps1
@@ -1,5 +1,5 @@
 # Loop thru prioritized list of VS Common Tools env var names; return the path value of the first env var found
-$envVarNames = @('VS120COMNTOOLS', 'VS110COMNTOOLS', 'VS100COMNTOOLS')
+$envVarNames = @('VS140COMNTOOLS', 'VS130COMNTOOLS', 'VS120COMNTOOLS', 'VS110COMNTOOLS', 'VS100COMNTOOLS')
 $vsCmnToolsPath = ''
 
 foreach ($evar in $envVarNames ) {
