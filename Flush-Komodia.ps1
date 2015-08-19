ae03cb6e51c5bef22b403cc94b2faf89955e9390
J Burnett <jburnett@altamodatech.com>
2015-08-19 17:56:32 -0400

    Created

---
+++ Flush-Komodia.ps1
@@ -0,0 +1,21 @@
+﻿# Open certificate store
+push-location cert:
+
+# Iterate through top level (eg., CurrentUser, LocalMachine)
+gci | %{
+
+Write-Host "Investigating "$_.Location
+
+    # Iterate through cert containers
+    gci | %{
+
+    Write-Host "   Investigating "$_.Name
+
+        # Find certificate
+        $thumbprint = 'CDD4EEAE6000AC7F40C3802C171E30148030C072'
+        gci -Path $thumbprint -ErrorAction SilentlyContinue
+    }    
+
+}
+
+pop-location
