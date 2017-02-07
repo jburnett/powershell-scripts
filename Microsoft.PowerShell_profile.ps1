eb955a59256c052bfbad5a469df4584cfde8cc75
jburnett <jburnett@altamodatech.com>
2017-02-07 11:43:41 -0500

    Update Posh-Git path

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -22,7 +22,7 @@ if (test-path $gitToolsRoot) {
 ### Load posh-git profile.
 if(Test-Path Function:\Prompt) {Rename-Item Function:\Prompt PrePoshGitPrompt -Force}
 # Load posh-git example profile (defines global prompt function)
-. 'C:\tools\poshgit\dahlbyk-posh-git-e8dd9f2\profile.example.ps1'
+. 'C:\tools\poshgit\dahlbyk-posh-git-fba883f\profile.example.ps1'
 Rename-Item Function:\Prompt PoshGitPrompt -Force
 function Prompt() {if(Test-Path Function:\PrePoshGitPrompt){++$global:poshScope; New-Item function:\script:Write-host -value "param([object] `$object, `$backgroundColor, `$foregroundColor, [switch] `$nonewline) " -Force | Out-Null;$private:p = PrePoshGitPrompt; if(--$global:poshScope -eq 0) {Remove-Item function:\Write-Host -Force}}PoshGitPrompt}
 
