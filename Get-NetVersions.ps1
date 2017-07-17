9178f54edf47e9b3253ee76c3f79ce6c566c4eec
jburnett <jburnett@altamodatech.com>
2017-07-16 23:14:38 -0400

    Updated specific version names

--- Get-NetVersions.ps1
+++ Get-NetVersions.ps1
@@ -1,12 +1,29 @@
 function getNetFriendlyName($netRelease) {
 		switch ($netRelease) {
 			378389 { return '.NET Framework 4.5.0' }
-			378675 { return '.NET Framework 4.5.1' } # included in Windows 8.1
-			378758 { return '.NET Framework 4.5.1' }
+			378675 { return '.NET Framework 4.5.1 (Windows 8.1 or Windows Server 2012 R2)' }
+			378758 { return '.NET Framework 4.5.1 (Windows 8, Windows 7 SP1, or Windows Vista SP2)' }
 			379893 { return '.NET Framework 4.5.2' }
+			393295 { return '.NET Framework 4.6.0 (Windows 10)' }
+			393297 { return '.NET Framework 4.6.0 (non-Windows 10)' }
+			393295 { return '.NET Framework 4.6.1 (Windows 10)' }
+			394271 { return '.NET Framework 4.6.1 (non-Windows 10)' }
+			394802 { return '.NET Framework 4.6.2 (Windows 10)' }
+			394806 { return '.NET Framework 4.6.2 (non-Windows 10)' }
 		}
 }
 
+# info from https://msdn.microsoft.com/en-us/library/hh925568(v=vs.110).aspx
+# .NET Framework 4.5	378389
+# .NET Framework 4.5.1 installed with Windows 8.1	378675
+# .NET Framework 4.5.1 installed on Windows 8, Windows 7 SP1, or Windows Vista SP2	378758
+# .NET Framework 4.5.2	379893
+# .NET Framework 4.6 installed with Windows 10	393295
+# .NET Framework 4.6 installed on all other Windows OS versions	393297
+# .NET Framework 4.6.1 installed on Windows 10	394254
+# .NET Framework 4.6.1 installed on all other Windows OS versions	394271
+# .NET Framework 4.6.2 installed on Windows 10 Anniversary Update	394802
+# .NET Framework 4.6.2 installed on all other Windows OS versions	394806
 
 function getNetNameOfKey([Microsoft.Win32.RegistryKey]$regKey) {
 
