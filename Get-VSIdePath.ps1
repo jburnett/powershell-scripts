5d3db18bd86422165901b496da838e2e8eb77323
J Burnett <jburnett@appliedsystems.com>
2017-07-17 18:42:39 -0400

    Add support for find VS2017 IDE path

--- Get-VSIdePath.ps1
+++ Get-VSIdePath.ps1
@@ -4,4 +4,15 @@ $vsCmnToolsPath = Get-VSCommonToolsPath.ps1
 if ($vsCmnToolsPath -and (test-path $vsCmnToolsPath)) {
 		$vsRootPath = gci IDE -path ($vsCmnToolsPath.Parent.FullName)
 }
-return $vsRootPath.FullName
+elseif (Test-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" ) {
+	$vsRootPath = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -property installationPath
+	$vsRootPath += "/Common7/IDE"
+}
+
+return $vsRootPath
+
+
+#----------------------------------------------------------------------------------------------------
+# History
+#	07/17/2017	Add support for VS2017's vswhere tool (IOW, dropped VSCOMNTOOLS env var)
+#----------------------------------------------------------------------------------------------------
