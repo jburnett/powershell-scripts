3816c9df28f9b164db9e0281131845892ff7fb33
jburnett <jburnett@altamodatech.com>
2019-02-15 14:13:03 -0500

    Show-Path finds duplicates

--- Show-Path.ps1
+++ Show-Path.ps1
@@ -1,2 +1,30 @@
+# JB, 2019/02/06: detect duplicate entries
 # J Burnett, 2012/09/04
-$env:path -split";"
+#param([switch]$Sort )
+
+$all = @()
+$unique = @{}
+
+$all = $env:path -split";"
+
+foreach ($p in $all) {
+    if ($unique.ContainsKey($p)) {
+        $unique[$p] += 1
+        Write-Verbose "#### $p is duplicate; $unique.Item($p)"
+    }
+    else {
+        $unique.Add("$p", 0)
+    }
+}
+
+Write-Host "==== Unique Paths ==="
+foreach ($p in $unique.GetEnumerator()) {
+    Write-Host $p.Key
+}
+
+Write-Host "==== Duplicate Paths ==="
+foreach ($p in $unique.GetEnumerator()) {
+    if ($p.Value -gt 0 ) {
+        Write-Host $p.Key   "`t("$p.Value "duplicates)"
+    }
+}
